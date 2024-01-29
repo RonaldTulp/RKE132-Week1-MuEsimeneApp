1d36b6b80ede36708d8fd6f2046b279bf51c0e59
Ronald <ronald.tulp@tktk.ee>
2024-01-29 23:53:35 +0200

    kommentaare

---
+++ MuEsimeneApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// <-- kommentaar
+
+//Küsi kasutaja nime
+//Tervita kasutajat nimepidi
+
+Console.WriteLine("Sisestage oma nimi:"); //output
+
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Tere, " + userName); //output
+
+//string interpolation
+Console.WriteLine($"Tere, {userName}"); //output
